f2727a686eb24f00e1d47e36f4a67e89317edb93
onura2 <50609671+onura2@users.noreply.github.com>
2019-05-14 16:13:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,19 +8,17 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === 'miaw') {
     msg.reply('miaw miaw miaw);
-  }
-              
+  }              
 });
 client.on('message', msg => {
-  if (msg.content === 'felix') {
-    msg.reply('mau OwO);
+  if (msg.content === 'felix ') {
+    msg.reply('mau OwO');
   }
-              
-});
-client.on('message', msg => {
-  if (msg.content === ' Zeytin felix istermisin') {
-    msg.reply('mau OwO);
-  }         
 });
 
+client.on('message', msg => {
+  if (msg.content === 'kedi ') {
+    msg.reply('mau OwO');
+  }
+});
 client.login(process.env.BOT_TOKEN);
